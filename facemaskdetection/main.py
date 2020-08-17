f8bad6f42f296942cf0668bdd225d5dc9a475e98
codebrain001 <abozejohn@gmail.com>
2020-08-17 18:02:40 +0100

    Refactory code

--- facemaskdetection/main.py
+++ facemaskdetection/main.py
@@ -1,27 +1,38 @@
-from drone_module.drone_config import Drone
-from detection_module.real_time_detection import get_facenet_and_masknet
-from detection_module.real_time_detection import detect_and_predict_mask
+from drone_config import Drone
+from detect_mask_video import get_facenet_masknet
+from detect_mask_video import detect_and_predict_mask
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import argparse
+import imutils
+import time
 import cv2
+import os
+import time
 
 if __name__ == '__main__':
-    # Getting the faceNet and maskNet
-    faceNet, maskNet = get_facenet_and_masknet()
+    
+    faceNet, maskNet = get_facenet_masknet()
 
     # Instatiating the Drone and setup operation
     drone_test = Drone(0, 640, 480, 0, 0, 0, 0, 0)
     status = drone_test.get_status()
     drone_test.get_stream()
+    time.sleep(2.0)
+
 
     # loop over the frames from the video stream
     while True:
         # grab the frame from the threaded video stream and resize it
         # to have a maximum width of 400 pixels
-        frame = drone_test.get_frame()
-        #frame = imutils.resize(frame, width=400)
+        my_frame = drone_test.get_frame()
+        # frame = imutils.resize(frame, width=400)
 
         # detect faces in the frame and determine if they are wearing a
         # face mask or not
-        locs, preds = detect_and_predict_mask(frame, faceNet, maskNet)
+        locs, preds = detect_and_predict_mask(my_frame, faceNet, maskNet)
 
         # loop over the detected face locations and their corresponding
         # locations
@@ -42,15 +53,15 @@ if __name__ == '__main__':
 
             # display the label and bounding box rectangle on the output
             # frame
-            cv2.putText(frame, label, (startX, startY - 10),
+            cv2.putText(my_frame, label, (startX, startY - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+            cv2.rectangle(my_frame, (startX, startY), (endX, endY), color, 2)
 
             # get snapshots of individuals without facemask
             # drone_test.get_snap_shots(label, frame)
 
         # show the output frame
-        cv2.imshow("Frame", frame)
+        cv2.imshow("Frame", my_frame)
         key = cv2.waitKey(1) & 0xFF
 
         # if the `q` key was pressed, break from the loop
